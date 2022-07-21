fad3e8a7333d0a17567ab4a614595b897585d75c
gboxs <gboxs@gmail.com>
2022-07-21 10:01:25 +0700

    update default setting.py

---
+++ settings/oauth.py
@@ -0,0 +1,25 @@
+
+INSTALLED_APPS += [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'oauth2_provider',
+    'users',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+AUTH_USER_MODEL='users.User'
+LOGIN_URL='/admin/login/'
